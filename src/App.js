0164abb23b3e7749847d82dc919e58fe28ca5bf8
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-31 17:49:49 +0500

    Create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UseReducer from './components/UseReducer';
+
+function App() {
+  return (
+    <div className="App">
+      <UseReducer />
+    </div>
+  );
+}
+
+export default App;
